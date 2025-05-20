d7294ac92607e3347acc7d2a800f3f25d4d275a4
ShamoonAbbas <shamoonsoft@gmail.com>
2025-05-20 14:18:44 +0500

    v2.5

--- todo_backend/server.js
+++ todo_backend/server.js
@@ -8,6 +8,21 @@ const TodoModel = require('./models/Todo');
 
 const app = express();
 
+// Health check endpoint
+app.get('/health', (req, res) => {
+    const healthcheck = {
+        uptime: process.uptime(),
+        message: 'OK',
+        timestamp: Date.now()
+    };
+    try {
+        res.send(healthcheck);
+    } catch (e) {
+        healthcheck.message = e;
+        res.status(503).send();
+    }
+});
+
 // Security middleware
 app.use(helmet()); // Adds various HTTP headers for security
 
